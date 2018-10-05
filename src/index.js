bd7410502109c211145bf9596e1658eeff5340ca
Iqbal Fasri <iqbalx64@gmail.com>
2018-10-05 15:44:30 +0700

    Change BrowserRouter to Hashrouter

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './App.css';
 import App from './App';
 import registerServiceWorker, { unregister } from './registerServiceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  (<BrowserRouter>
+  (<HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   ), document.getElementById('root')
 );
 unregister();
